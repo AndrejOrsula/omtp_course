efb3d4551ccc411c896ed40c13c6a8e0e4cf0203
Nitrow <printz.a91@gmail.com>
2020-03-09 16:48:24 +0100

    Add finished urdf_tutorial

--- omtp_lecture1/launch/r2d2.launch.py
+++ omtp_lecture1/launch/r2d2.launch.py
@@ -1,4 +1,4 @@
-"""Launch of a Gazebo instance with a model defined by xacro"""
+"""Launch of a Gazebo instance with a model defined by URDF"""
 
 import os
 from ament_index_python.packages import get_package_share_directory
@@ -12,11 +12,11 @@ from launch.substitutions import ThisLaunchFileDir
 
 
 def generate_launch_description():
-
+    # Path to URDF defining Gazebo model
+    urdf_path = os.path.join(get_package_share_directory(
+        'urdf_tutorial'), 'urdf', 'r2r2.urdf')
 
     return LaunchDescription([
-
-
         # Start Gazebo instance
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(os.path.join(get_package_share_directory(
@@ -24,8 +24,9 @@ def generate_launch_description():
         ),
 
         # Spawn entity with corresponding urdf file
+        # Path needs to be static otherwise `spawn_entity.py` return an error about not recognizing `--ros-args`
         Node(package='gazebo_ros',
              node_executable='spawn_entity.py',
-             arguments=['-entity', 'OMTP', '-file', '../omtp_course/urdf_tutorial/urdf/05-visual.urdf'],
+             arguments=['-entity', 'r2d2', '-file', '/TODO/path/to/urdf_tutorial/urdf/r2d2.urdf'],
              output='screen'),
     ])
