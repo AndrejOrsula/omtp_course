ca73ca85b97f12b1fe54dccb65d7b680bd7201be
Andrej Orsula <orsula.andrej@gmail.com>
2020-06-02 19:57:50 +0200

    Add graphics for lecture 7

--- lecture_7/README.md
+++ lecture_7/README.md
@@ -1,1 +1,20 @@
 # Lecture 7 - Behavior Design with State Machines
+
+This lecture provides an example of designing behaviour with state machines. FlexBE is utilised to design a state machine of manipulating a virtual Gazebo object by the use of MoveIt.
+
+![conveyor_object_mapinulation](graphics/conveyor_object_mapinulation.gif)
+
+![state_machine](graphics/state_machine.png)
+
+## Usage
+Once build, the assignment can be executed with the following instructions.
+
+First, setup the OMTP factory environment in Gazebo.
+```bash
+roslaunch omtp_gazebo omtp_pick_demo.launch
+```
+
+Then run FlexBE in another terminal, load the behaviour via GUI and run it.
+```bash
+roslaunch flexbe_app flexbe_full.launch
+```
--- lecture_7/lecture_7_behaviors/lecture_7_flexbe_behaviors/src/lecture_7_flexbe_behaviors/omtp_conveyor_pick_place_sm.py
+++ lecture_7/lecture_7_behaviors/lecture_7_flexbe_behaviors/src/lecture_7_flexbe_behaviors/omtp_conveyor_pick_place_sm.py
@@ -8,10 +8,12 @@
 ###########################################################
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
-from flexbe_manipulation_states.moveit_to_joints_dyn_state import MoveitToJointsDynState as flexbe_manipulation_states__MoveitToJointsDynState
+from omtp_factory_flexbe_states.set_conveyor_power_state import SetConveyorPowerState
 from omtp_factory_flexbe_states.detect_part_camera_state import DetectPartCameraState
+from flexbe_manipulation_states.moveit_to_joints_dyn_state import MoveitToJointsDynState as flexbe_manipulation_states__MoveitToJointsDynState
 from omtp_factory_flexbe_states.compute_grasp_state import ComputeGraspState
 from omtp_factory_flexbe_states.vacuum_gripper_control_state import VacuumGripperControlState
+from omtp_factory_flexbe_states.control_feeder_state import ControlFeederState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -20,7 +22,7 @@ from omtp_factory_flexbe_states.vacuum_gripper_control_state import VacuumGrippe
 
 '''
 Created on Thu May 07 2020
-@author: Andrej Orsula and Asger P. Madsen
+@author: Andrej Orsula
 '''
 class omtp_conveyor_pick_placeSM(Behavior):
 	'''
@@ -51,7 +53,7 @@ class omtp_conveyor_pick_placeSM(Behavior):
 		robot1_home = [1.5708, -1.5708, 1.5708, -1.5708, -1.5708, 0]
 		robot1_pregrasp = [1.5708, -1.5708, 0.785398, -1.5708, -1.5708, 0]
 		robot1_place = [1.5708, -1.5708, -1.5708, -1.5708, -1.5708, 0]
-		# x:1453 y:843, x:25 y:857
+		# x:1746 y:859, x:310 y:824
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.robot1_joint_names = robot1_joint_names
 		_state_machine.userdata.robot1_home = robot1_home
@@ -60,6 +62,7 @@ class omtp_conveyor_pick_placeSM(Behavior):
 		_state_machine.userdata.pick_configuration = []
 		_state_machine.userdata.robot1_place = robot1_place
 		_state_machine.userdata.computed_joint_names = []
+		_state_machine.userdata.speed = 100.0
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -68,66 +71,94 @@ class omtp_conveyor_pick_placeSM(Behavior):
 
 
 		with _state_machine:
-			# x:41 y:30
-			OperatableStateMachine.add('move_home',
-										flexbe_manipulation_states__MoveitToJointsDynState(move_group=robot1_move_group, action_topic='/move_group'),
-										transitions={'reached': 'detect_part_on_conveyor', 'planning_failed': 'failed', 'control_failed': 'failed'},
-										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
-										remapping={'joint_values': 'robot1_home', 'joint_names': 'robot1_joint_names'})
+			# x:52 y:25
+			OperatableStateMachine.add('start_conveyor',
+										SetConveyorPowerState(stop=False),
+										transitions={'succeeded': 'start_feeder', 'failed': 'failed'},
+										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'speed': 'speed'})
 
-			# x:246 y:53
+			# x:697 y:72
 			OperatableStateMachine.add('detect_part_on_conveyor',
 										DetectPartCameraState(ref_frame='robot1_base_link', camera_topic='/omtp/logical_camera', camera_frame='logical_camera_frame'),
-										transitions={'continue': 'compute_pregrasp', 'failed': 'failed'},
+										transitions={'continue': 'move_pregrasp', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'pose': 'part_pose'})
 
-			# x:442 y:71
-			OperatableStateMachine.add('compute_pregrasp',
-										ComputeGraspState(group=robot1_move_group, offset=0.175, joint_names=robot1_joint_names, tool_link='vacuum_gripper1_suction_cup', rotation=3.1415),
-										transitions={'continue': 'move_pregrasp', 'failed': 'failed'},
-										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
-										remapping={'pose': 'part_pose', 'joint_values': 'pick_configuration', 'joint_names': 'computed_joint_names'})
-
-			# x:640 y:118
-			OperatableStateMachine.add('move_pregrasp',
+			# x:1554 y:162
+			OperatableStateMachine.add('move_grasp_approach',
 										flexbe_manipulation_states__MoveitToJointsDynState(move_group=robot1_move_group, action_topic='/move_group'),
-										transitions={'reached': 'compute_grasp', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										transitions={'reached': 'detect_part_on_conveyor_3', 'planning_failed': 'failed', 'control_failed': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
 										remapping={'joint_values': 'pick_configuration', 'joint_names': 'computed_joint_names'})
 
-			# x:1084 y:209
+			# x:1670 y:467
 			OperatableStateMachine.add('move_grasp',
 										flexbe_manipulation_states__MoveitToJointsDynState(move_group=robot1_move_group, action_topic='/move_group'),
 										transitions={'reached': 'activate_gripper', 'planning_failed': 'failed', 'control_failed': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
 										remapping={'joint_values': 'pick_configuration', 'joint_names': 'computed_joint_names'})
 
-			# x:854 y:160
+			# x:1653 y:359
 			OperatableStateMachine.add('compute_grasp',
-										ComputeGraspState(group=robot1_move_group, offset=0.15, joint_names=robot1_joint_names, tool_link='vacuum_gripper1_suction_cup', rotation=3.1415),
+										ComputeGraspState(group=robot1_move_group, offset=0.16, joint_names=robot1_joint_names, tool_link='vacuum_gripper1_suction_cup', rotation=3.1415),
 										transitions={'continue': 'move_grasp', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'pose': 'part_pose', 'joint_values': 'pick_configuration', 'joint_names': 'computed_joint_names'})
 
-			# x:1308 y:264
+			# x:1681 y:581
 			OperatableStateMachine.add('activate_gripper',
 										VacuumGripperControlState(enable='true', service_name='/gripper1/control'),
 										transitions={'continue': 'move_place', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:1319 y:377
+			# x:1687 y:708
 			OperatableStateMachine.add('move_place',
 										flexbe_manipulation_states__MoveitToJointsDynState(move_group=robot1_move_group, action_topic='/move_group'),
-										transitions={'reached': 'activate_gripper_2', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
 										remapping={'joint_values': 'robot1_place', 'joint_names': 'robot1_joint_names'})
 
-			# x:1314 y:485
-			OperatableStateMachine.add('activate_gripper_2',
-										VacuumGripperControlState(enable='false', service_name='/gripper1/control'),
-										transitions={'continue': 'finished', 'failed': 'failed'},
-										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
+			# x:498 y:58
+			OperatableStateMachine.add('move_home',
+										flexbe_manipulation_states__MoveitToJointsDynState(move_group=robot1_move_group, action_topic='/move_group'),
+										transitions={'reached': 'detect_part_on_conveyor', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'joint_values': 'robot1_home', 'joint_names': 'robot1_joint_names'})
+
+			# x:1332 y:123
+			OperatableStateMachine.add('compute_grasp_approach',
+										ComputeGraspState(group=robot1_move_group, offset=0.5, joint_names=robot1_joint_names, tool_link='vacuum_gripper1_suction_cup', rotation=3.1415),
+										transitions={'continue': 'move_grasp_approach', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'pose': 'part_pose', 'joint_values': 'pick_configuration', 'joint_names': 'computed_joint_names'})
+
+			# x:283 y:40
+			OperatableStateMachine.add('start_feeder',
+										ControlFeederState(activation=True),
+										transitions={'succeeded': 'move_home', 'failed': 'failed'},
+										autonomy={'succeeded': Autonomy.Off, 'failed': Autonomy.Off})
+
+			# x:904 y:88
+			OperatableStateMachine.add('move_pregrasp',
+										flexbe_manipulation_states__MoveitToJointsDynState(move_group=robot1_move_group, action_topic='/move_group'),
+										transitions={'reached': 'detect_part_on_conveyor_2', 'planning_failed': 'failed', 'control_failed': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
+										remapping={'joint_values': 'robot1_pregrasp', 'joint_names': 'robot1_joint_names'})
+
+			# x:1108 y:104
+			OperatableStateMachine.add('detect_part_on_conveyor_2',
+										DetectPartCameraState(ref_frame='robot1_base_link', camera_topic='/omtp/logical_camera', camera_frame='logical_camera_frame'),
+										transitions={'continue': 'compute_grasp_approach', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'pose': 'part_pose'})
+
+			# x:1627 y:260
+			OperatableStateMachine.add('detect_part_on_conveyor_3',
+										DetectPartCameraState(ref_frame='robot1_base_link', camera_topic='/omtp/logical_camera', camera_frame='logical_camera_frame'),
+										transitions={'continue': 'compute_grasp', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'pose': 'part_pose'})
 
 
 		return _state_machine
